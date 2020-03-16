1c60b870f31b5726df367a463a49942ca1ca875f
Mauricio Robayo <hi@mauriciorobayo.com>
2020-03-15 21:25:43 -0500

    chore: initial search with memo

--- src/search.js
+++ src/search.js
@@ -1,20 +1,31 @@
 const twitterize = require('twitterize')
+const Memo = require('./memo')
 
-const t = twitterize({
-  api_key: process.env.TWITTER_API_KEY,
-  api_secret_key: process.env.TWITTER_API_SECRET_KEY,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-})
+const search = async () => {
+  const memo = new Memo()
+  const { max_id: maxId } = await memo.getLastExecInfo()
+  const t = twitterize({
+    api_key: process.env.TWITTER_API_KEY,
+    api_secret_key: process.env.TWITTER_API_SECRET_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  })
+  const options = {
+    requestMethod: 'GET',
+    endpoint: '/search/tweets.json',
+    queryParams: {
+      q: '@microverseinc -filter:retweets',
+      since_id: maxId,
+      result_type: 'recent',
+    },
+  }
 
-const options = {
-  requestMethod: 'GET',
-  endpoint: '/search/tweets.json',
-  queryParams: { q: '@microverseinc -filter:retweets' },
+  t(options)
+    .then(response => {
+      console.log(response)
+      memo.setLastExecInfo({ maxId: response.search_metadata.max_id })
+    })
+    .catch(console.log)
 }
 
-t(options)
-  .then(response => {
-    console.log(JSON.parse(response))
-  })
-  .catch(console.log)
+search()
